04683543e08a0115080fd086f1bdb751f92f9062
gustavo henrique spiess <gustavo.spiess@philips.com>
2019-06-20 02:49:49 -0300

    Logical Ring 1.1

--- connect_validator/token_ring.py
+++ connect_validator/token_ring.py
@@ -1,16 +1,18 @@
 class Token():
     def __init__(self, url, server_stack=[], response_stack=[]):
-        # TODO: docstring
+        """Inicializa o token com uma url, e opcionalmente recebendo a pilha de
+        servidores e de respostas"""
         self.url = url
         self.server_stack = server_stack
         self.response_stack = response_stack
 
     def append_server(self, server, response):
+        """Adiciona o servidor e a resposta à pilha do token"""
         self.server_stack.append(server)
         self.response_stack.append(response)
 
     def __repr__(self):
-        # TODO: docstring
+        """Cria uma expressão valorável do token para enviálo"""
         url = repr(self.url)
         server = repr(self.server_stack)
         response = repr(self.response_stack)
